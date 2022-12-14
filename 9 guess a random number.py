39cda16a1541663e4a217c8eaffe8202832de162
NeitCreates <12614438+NeitCreates@users.noreply.github.com>
2022-12-14 06:01:55 +0400

    Added stupidity counter to 9 guess a random number
    You make stupid mistakes, it increases. 3 variants of end phrase depending on the counter available.

--- 9 guess a random number.py
+++ 9 guess a random number.py
@@ -4,11 +4,13 @@ import random
 def check_input(input_prompt):
 	'''Function to input a value and check if it's int and > 0'''
 
+	global stpd_counter
 	while True:
 		try:
 			value = int(input(input_prompt))
 		except ValueError:
 			print("Input a number, not words, you asshole!")
+			stpd_counter += 1
 			continue
 		if value < 0:
 			print("The value should be > 0! Input a correct one!")
@@ -18,6 +20,8 @@ def check_input(input_prompt):
 	return value
 
 
+stpd_counter = 0
+
 print("Input the interval you want the number to be in")
 
 min_number = check_input("Input min number: ")
@@ -25,6 +29,7 @@ while True:
 	max_number = check_input("Input max number: ")
 	if min_number > max_number:
 		print("Your min number cannot be greater than your max. Did you even attend school, idiot?")
+		stpd_counter += 3
 		continue
 	else:
 		break
@@ -36,6 +41,7 @@ while True:
 	guess_num = check_input("Input your guess: ")
 	if not min_number <= guess_num <= max_number:
 		print("Are you retarded or what? You've just specified the range and now you're guessing out of it?!")
+		stpd_counter += 3
 		continue
 	elif guess_num != secret_numb:
 		if guess_num < secret_numb:
@@ -44,10 +50,17 @@ while True:
 			print("Not quite, go lower!")
 		continue
 	elif guess_num == secret_numb:
-		print(f"Great, you got it, the number is {secret_numb}")
-		break
+		if stpd_counter <= 4:
+			print(f"Great, you got it, the number is {secret_numb}")
+			break
+		elif 5 <= stpd_counter <= 10:
+			print(f"You did it, the number is {secret_numb}. However you're rather dumb and fucked up a lot.")
+			break
+		elif stpd_counter > 10:
+			print(f"The number is {secret_numb} and it's sheer luck you got it. I've never seen such a stupid and useless humanbeing. I advise you to go and die right away.")
+			break
+
 
 '''
-add stupidity counter - diff message depending on the number of fuckups
 different functions for input checks
 '''
